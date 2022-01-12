c8e14353f282cdb00ac54b9cdc0dcd21fff8f20a
Kirill Zhukov <kdzhukov@sbercloud.ru>
2022-01-12 17:59:23 +0300

    Added CartAddressForm component

--- src/frontend/src/views/Cart.vue
+++ src/frontend/src/views/Cart.vue
@@ -9,53 +9,10 @@
 
           <!-- <div class="sheet cart__empty">
           <p>В корзине нет ни одного товара</p>
-        </div> -->
+         </div> -->
           <CartPizzaList />
           <CartAdditionalItems />
-
-          <div class="cart__form">
-            <div class="cart-form">
-              <label class="cart-form__select">
-                <span class="cart-form__label">Получение заказа:</span>
-
-                <select name="test" class="select">
-                  <option value="1">Заберу сам</option>
-                  <option value="2">Новый адрес</option>
-                  <option value="3">Дом</option>
-                </select>
-              </label>
-
-              <label class="input input--big-label">
-                <span>Контактный телефон:</span>
-                <input type="text" name="tel" placeholder="+7 999-999-99-99" />
-              </label>
-
-              <div class="cart-form__address">
-                <span class="cart-form__label">Новый адрес:</span>
-
-                <div class="cart-form__input">
-                  <label class="input">
-                    <span>Улица*</span>
-                    <input type="text" name="street" />
-                  </label>
-                </div>
-
-                <div class="cart-form__input cart-form__input--small">
-                  <label class="input">
-                    <span>Дом*</span>
-                    <input type="text" name="house" />
-                  </label>
-                </div>
-
-                <div class="cart-form__input cart-form__input--small">
-                  <label class="input">
-                    <span>Квартира</span>
-                    <input type="text" name="apartment" />
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
+          <CartAddressForm />
         </div>
       </main>
       <section class="footer">
@@ -83,11 +40,13 @@
 import { mapGetters } from "vuex";
 import CartPizzaList from "@/modules/cart/components/CartPizzaList";
 import CartAdditionalItems from "@/modules/cart/components/CartAdditionalItems";
+import CartAddressForm from "@/modules/cart/components/CartAddressForm";
 export default {
   name: "Cart",
   components: {
     CartPizzaList,
     CartAdditionalItems,
+    CartAddressForm,
   },
   computed: {
     ...mapGetters("Cart", ["cartPrice"]),
