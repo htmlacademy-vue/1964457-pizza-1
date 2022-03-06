5e6d469fa73fa5e97b3b38c98232394143b81919
Kirill Zhukov <kdzhukov@sbercloud.ru>
2022-03-06 09:58:47 +0300

    AddLayoutMainWithSidebar.vue: Added class layout__link--active to active link

--- src/frontend/src/layouts/AppLayoutMainWithSidebar.vue
+++ src/frontend/src/layouts/AppLayoutMainWithSidebar.vue
@@ -11,10 +11,20 @@
             height="40"
           />
         </router-link>
-        <router-link to="/orders" class="layout__link">
+        <router-link
+          to="/orders"
+          class="layout__link"
+          active-class="layout__link--active"
+        >
           История заказов
         </router-link>
-        <router-link to="/profile" class="layout__link">Мои данные</router-link>
+        <router-link
+          to="/profile"
+          class="layout__link"
+          active-class="layout__link--active"
+        >
+          Мои данные
+        </router-link>
       </div>
       <slot />
     </main>
