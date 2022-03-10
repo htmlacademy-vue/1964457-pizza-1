a7f7b9cef4da9e74283299e29f3d9d632a7a51ce
Kirill Zhukov <kdzhukov@sbercloud.ru>
2022-03-10 12:14:02 +0300

    Added tests fro ItemCounter.vue

---
+++ src/frontend/src/common/components/__tests__/ItemCounter.spec.js
@@ -0,0 +1,66 @@
+import { mount } from "@vue/test-utils";
+import ItemCounter from "@/common/components/ItemCounter";
+
+describe("ItemCounter", () => {
+  const wrapper = mount(ItemCounter, {
+    propsData: {
+      min: 0,
+      current: 0,
+      max: 1,
+    },
+  });
+  const minus_button = wrapper.find(".counter__button--minus");
+  const plus_button = wrapper.find(".counter__button--plus");
+  const input = wrapper.find(".counter__input");
+
+  it("Minus button is disabled", () => {
+    expect(minus_button.element.disabled).toBe(true);
+  });
+
+  it("Plus button is enabled", () => {
+    expect(plus_button.element.disabled).toBe(false);
+  });
+
+  it("Input element is readonly", () => {
+    expect(input.element.readOnly).toBe(true);
+  });
+
+  it("Input element has currect inital value", () => {
+    expect(input.element.value).toBe("0");
+  });
+
+  it("Click on plus button emits 'increase'", () => {
+    plus_button.trigger("click");
+    expect(wrapper.emitted().increase).toBeTruthy();
+  });
+});
+
+describe("ItemCounter with max values", () => {
+  const wrapper = mount(ItemCounter, {
+    propsData: {
+      min: 0,
+      current: 1,
+      max: 1,
+    },
+  });
+  const minus_button = wrapper.find(".counter__button--minus");
+  const plus_button = wrapper.find(".counter__button--plus");
+  const input = wrapper.find(".counter__input");
+
+  it("Minus button is enabled", () => {
+    expect(minus_button.element.disabled).toBe(false);
+  });
+
+  it("Plus button is disabled", () => {
+    expect(plus_button.element.disabled).toBe(true);
+  });
+
+  it("Input element has currect inital value", () => {
+    expect(input.element.value).toBe("1");
+  });
+
+  it("Click on minus button emits 'decrease'", () => {
+    minus_button.trigger("click");
+    expect(wrapper.emitted().decrease).toBeTruthy();
+  });
+});
