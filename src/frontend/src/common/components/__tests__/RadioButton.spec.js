afd462c234e9d06461b71e41c8c9ecc2f0417423
Kirill Zhukov <kdzhukov@sbercloud.ru>
2022-03-16 15:14:10 +0300

    Added beforeEach/afterEach to common/componetns

--- src/frontend/src/common/components/__tests__/RadioButton.spec.js
+++ src/frontend/src/common/components/__tests__/RadioButton.spec.js
@@ -1,30 +1,41 @@
-import { mount } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import RadioButton from "@/common/components/RadioButton";
 
 describe("RadioButton", () => {
-  const wrapper = mount(RadioButton, {
-    propsData: {
-      className: "test-class",
-      name: "test-name",
-      value: "test-value",
-    },
+  let wrapper;
+  const propsData = {
+    className: "test-class",
+    name: "test-name",
+    value: "test-value",
+  };
+  const localVue = createLocalVue();
+
+  const createComponent = (options) => {
+    wrapper = mount(RadioButton, options);
+  };
+
+  beforeEach(() => {
+    createComponent({ propsData, localVue });
   });
-  const input = wrapper.find("input");
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
 
   it("has correct class", () => {
-    expect(input.attributes("class")).toBe("test-class");
+    expect(wrapper.find("input").attributes("class")).toBe("test-class");
   });
 
   it("has correct name", () => {
-    expect(input.attributes("name")).toBe("test-name");
+    expect(wrapper.find("input").attributes("name")).toBe("test-name");
   });
 
   it("has correct value", () => {
-    expect(input.attributes("value")).toBe("test-value");
+    expect(wrapper.find("input").attributes("value")).toBe("test-value");
   });
 
   it("emits updateSelected", () => {
-    input.trigger("click");
+    wrapper.find("input").trigger("click");
     expect(wrapper.emitted().updateSelected).toBeTruthy();
   });
 });
--- src/frontend/src/common/components/__tests__/SelectorItem.spec.js
+++ src/frontend/src/common/components/__tests__/SelectorItem.spec.js
@@ -1,25 +1,38 @@
-import { mount } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import SelectorItem from "@/common/components/SelectorItem";
 
 describe("SelectorItem", () => {
-  const wrapper = mount(SelectorItem, {
-    propsData: {
-      ingredient: {
-        id: 1,
-        name: "Тест",
-        image: "/public/img/filling/test.svg",
-        price: 33,
-        count: 0,
-      },
-      draggable: true,
+  let wrapper;
+  const propsData = {
+    ingredient: {
+      id: 1,
+      name: "Тест",
+      image: "/public/img/filling/test.svg",
+      price: 33,
+      count: 0,
     },
+    draggable: true,
+  };
+
+  const localVue = createLocalVue();
+
+  const createComponent = (options) => {
+    wrapper = mount(SelectorItem, options);
+  };
+
+  beforeEach(() => {
+    createComponent({ propsData, localVue });
   });
-  const span = wrapper.find("span");
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
   it("has correct class", () => {
-    expect(span.attributes("class")).toContain("filling--test");
+    expect(wrapper.find("span").attributes("class")).toContain("filling--test");
   });
 
   it("has correct name", () => {
-    expect(span.text()).toBe("Тест");
+    expect(wrapper.find("span").text()).toBe("Тест");
   });
 });
